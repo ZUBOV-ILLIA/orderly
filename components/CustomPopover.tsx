5b2d444caacc3aacaef3d5883ea5d8c98c3aa046
ZUBOV ILLIA <zubovillia@gmail.com>
2024-12-29 13:16:56 +0300

    upgrade custom popover using react portal

--- components/CustomPopover.tsx
+++ components/CustomPopover.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 export default function CustomPopover({
   content,
@@ -18,9 +19,19 @@ export default function CustomPopover({
   behaviour?: "onHover" | "onClick";
 }) {
   const [isVisible, setIsVisible] = useState(false);
+  const [tooltipPosition, setTooltipPosition] = useState<{
+    top: number;
+    left: number;
+  } | null>(null);
 
-  function onMouseEnter() {
+  function onMouseEnter(event: React.MouseEvent) {
     if (behaviour === "onHover") {
+      const rect = event.currentTarget.getBoundingClientRect();
+
+      setTooltipPosition({
+        top: rect.top + window.scrollY,
+        left: rect.left + window.scrollX,
+      });
       setIsVisible(true);
     }
   }
@@ -31,31 +42,29 @@ export default function CustomPopover({
     }
   }
 
-  function onClick() {
-    if (behaviour === "onClick") {
-      setIsVisible(!isVisible);
-    }
-  }
-
   return (
     <div
       className={className}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
-      onClick={onClick}
     >
-      <div className="position-relative">
-        {isVisible && content.length > maxLength && (
+      {isVisible &&
+        content.length > maxLength &&
+        tooltipPosition &&
+        ReactDOM.createPortal(
           <div
-            className="p-2 position-absolute bottom-100 z-3 bg-white border rounded-2 shadow animate__animated animate__fadeIn animate__faster"
+            className="p-2 position-fixed z-3 bg-white border rounded-2 shadow animate__animated animate__fadeIn animate__faster"
             style={{
               maxWidth,
+              top: tooltipPosition.top,
+              left: tooltipPosition.left,
+              transform: "translateY(-100%)",
             }}
           >
             {content}
-          </div>
+          </div>,
+          document.body
         )}
-      </div>
       {children}
     </div>
   );
